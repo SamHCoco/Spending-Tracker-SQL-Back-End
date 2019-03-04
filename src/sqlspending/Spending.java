74597c970b3d237868a255574c379c4df1bb7592
SamHCoco <42847052+SamHCoco@users.noreply.github.com>
2019-03-04 22:57:53 +0000

    Method for calculating summary stats complete

--- src/sqlspending/Spending.java
+++ src/sqlspending/Spending.java
@@ -56,7 +56,7 @@ public class Spending {
     // finds the week ( from 1 - 4) of the current month
     public static int weekOfCurrentMonth(){
         Calendar calendar = Calendar.getInstance();
-        return calendar.get(Calendar.WEEK_OF_MONTH) + 1; // + 1 since first week = 0
+        return calendar.get(Calendar.WEEK_OF_MONTH);
     }
 
 
@@ -74,20 +74,81 @@ public class Spending {
         return monthSpendingTotal;
     }
 
-    public static double calculateWeekTotal(ArrayList<Spending> records){
+    public static double calculateWeekTotal(ArrayList<Spending> record){
         String currentMonth = getCurrentMonth();
         String month;
         int weekOfCurrentMonth = Spending.weekOfCurrentMonth();
         int weekOfMonth;
         double weekTotal = 0;
-        for(int i = 0; i < records.size(); i++){
-            month = records.get(i).getDate();
-            weekOfMonth = records.get(i).getWeekOfMonth();
+        for(int i = 0; i < record.size(); i++){
+            month = record.get(i).getDate();
+            weekOfMonth = record.get(i).getWeekOfMonth();
             if(currentMonth.equals(month.substring(3, 5)) && weekOfCurrentMonth == weekOfMonth){
-                    weekTotal += records.get(i).getAmount();
+                    weekTotal += record.get(i).getAmount();
             }
         }
         return weekTotal;
     }
 
+
+    public static double[] calculateStats(ArrayList<Spending> spending){
+        double[] categories = new double[6];
+        String currentMonth =  getCurrentMonth();
+        double monthTotal = calculateMonthTotal(spending);
+        for(int i = 0; i < spending.size(); i++){
+            if(spending.get(i).getDate().substring(3,5).equals(currentMonth)){
+                if(spending.get(i).getCategory().equals("FOOD")){
+                    categories[0] += spending.get(i).getAmount();
+
+                } else if(spending.get(i).getCategory().equals("TRANSPORT")){
+                    categories[1] += spending.get(i).getAmount();
+
+                } else if(spending.get(i).getCategory().equals("LEISURE")){
+                    categories[2] += spending.get(i).getAmount();
+
+                } else if(spending.get(i).getCategory().equals("CLOTHING")){
+                    categories[3] += spending.get(i).getAmount();
+
+                } else if(spending.get(i).getCategory().equals("HOUSING")){
+                    categories[4] += spending.get(i).getAmount();
+
+                } else if(spending.get(i).getCategory().equals("MISC.")){
+                    categories[5] += spending.get(i).getAmount();
+                }
+            }
+        }
+    // calculates (Amount spent on a category / total spending) * 100 to give category spending as % of total spending
+        for(int i = 0; i < 6; i++){
+            categories[i] = (categories[i] / monthTotal) * 100;
+        }
+        return categories;
+    }
+
+
+    public static void displayStats(double[] categories){
+        System.out.println("CATEGORY SPENDING SUMMARY STATISTICS");
+        for(int i = 0; i < categories.length; i++){
+            switch(i){
+                case 0:
+                    System.out.println("FOOD = " + categories[0] + "%");
+                    break;
+                case 1:
+                    System.out.println("TRANSPORT = " + categories[1] + "%");
+                    break;
+                case 2:
+                    System.out.println("LEISURE = " + categories[2] + "%");
+                    break;
+                case 3:
+                    System.out.println("CLOTHING = " + categories[3] + "%");
+                    break;
+                case 4:
+                    System.out.println("HOUSING = " + categories[4] + "%");
+                    break;
+                case 5:
+                    System.out.println("MISC. = " + categories[5] + "%");
+                    break;
+
+            }
+        }
+    }
 }
